fc413fb55e649f5d6164b920b033442987169545
arhan-cyber <floorgangforever007@gmail.com>
2025-02-09 11:40:15 +0530

    Create anagram.cpp
    checks if string inputs of chars are anagrams of each other

---
+++ anagram.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+bool isAnagram(int n, int m){
+
+    int sum=0, sumnew=0, product=1, prodnew=1;
+    for (int i=0; i< n; i++){
+        char c;
+        cin >> c;
+        if (c>=97){
+            c=c-32;
+        }
+        sum=sum+c;
+        product=product*c;
+    }
+    for (int i=0; i<m; i++){
+
+        char c;
+        cin >>c;
+        if (c>=97){
+            c=c-32;
+        }
+        sumnew=sumnew+c;
+        prodnew=prodnew*c;
+    }
+    if (sumnew==sum && product==prodnew && n==m){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+int main(){
+    int t;
+    cin >> t;
+    for (int j=0; j<t; j++){
+    int n, m;
+    cin >> n >> m;
+    cout << isAnagram(n, m);
+    }
+}
